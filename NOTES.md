329f3c5313c36556b4a91fc9e92411d4dbfe1063
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-06-17 12:22:27 +0200

    crud de usuario

--- NOTES.md
+++ NOTES.md
@@ -10,3 +10,15 @@ http://localhost:3000/users/1 [GET]
 
 Devuelve un usuario
 
+http://localhost:3000/users [POST]
+
+Crea un usuario
+
+http://localhost:3000/users/1 [PUT]
+
+Actualiza un usuario
+
+http://localhost:3000/users/1 [DELETE]
+
+Borra un usuario
+
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 interface User {
   id: string;
@@ -20,6 +20,50 @@ export class UsersController {
 
   @Get(':id')
   findUser(@Param('id') id: string) {
-    return this.users.find((user) => user.id === id);
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      return { error: 'User not found' };
+    }
+    return user;
+  }
+
+  @Post()
+  createUser(@Body() body: { name: string; email: string }) {
+    const newUser: User = {
+      id: (this.users.length + 1).toString(),
+      name: body.name,
+      email: body.email,
+    };
+
+    if (!newUser.name || !newUser.email) {
+      return { error: 'Name and email are required' };
+    }
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  @Put(':id')
+  updateUser(@Param('id') id: string, @Body() body: { name?: string; email?: string }) {
+    const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      return { error: 'User not found' };
+    }
+    if (body.name) {
+      user.name = body.name;
+    }
+    if (body.email) {
+      user.email = body.email;
+    }
+    return user;
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    const position = this.users.findIndex((user) => user.id === id);
+    if (position === -1) {
+      return { error: 'User not found' };
+    }
+    this.users.splice(position, 1); // eliminame esa posición del array y solamente elimina un elemento
+    return { message: 'User deleted successfully' };
   }
 }
