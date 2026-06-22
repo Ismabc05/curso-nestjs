ad239d29d59c186056451acbcde433ed6b8fc42b
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-06-22 13:01:28 +0200

    variables de entorno

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,17 +1,22 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
+import { Env } from './env.model';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private readonly usersService: UsersService,
+    private readonly configService: ConfigService<Env>,
   ) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    const myVar = this.configService.get('MY_VAR', { infer: true }); // Accedemos a la variable de entorno MY_VAR
+    const message = this.appService.getHello();
+    return `${message} - MY_VAR: ${myVar}`;
   }
 
   @Get('my-test')
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersController } from './users/users.controller';
 import { UsersService } from './users/users.service';
+import { ConfigModule } from '@nestjs/config/dist/config.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ], // Importamos el módulo de configuración para manejar variables de entorno de forma global
   controllers: [AppController, UsersController],
   providers: [AppService, UsersService],
 })
