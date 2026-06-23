2429812e04c292820127dd3f9a228c17b7556ee2
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-06-23 12:36:54 +0200

    modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-  ], // Importamos el módulo de configuración para manejar variables de entorno de forma global
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+    UsersModule,
+  ],
 })
 export class AppModule {}
---
+++ src/users/users.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+
+@Module({
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService], // Exportamos UsersService para que pueda ser utilizado en otros módulos
+})
+export class UsersModule {}
