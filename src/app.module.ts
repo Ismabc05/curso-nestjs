de215039a840f5b7ea95b144aaafcd701da0a649
Ismabc05 <ismaelbedmarcejas@gmail.com>
2026-07-14 11:32:06 +0200

    configuracion dde render

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,6 +25,9 @@ import { AiModule } from './ai/ai.module';
         username: configService.get('POSTGRES_USER', { infer: true }),
         password: configService.get('POSTGRES_PASSWORD', { infer: true }),
         database: configService.get('POSTGRES_DB', { infer: true }),
+        ssl: {
+          rejectUnauthorized: false,
+        },
         autoLoadEntities: true,
         synchronize: false,
       }),
